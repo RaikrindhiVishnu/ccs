f78aabdff3f4ef6df2ad4e3b60ddf3577c9abead
Harish.K <kollatiharish@gmail.com>
2026-04-28 12:36:23 +0530

    merge conflicts

--- src/app/router/routes.config.tsx
+++ src/app/router/routes.config.tsx
@@ -4,11 +4,17 @@ import type { RouteObject } from 'react-router-dom';
 const LoginPage = lazy(() => import('../../features/auth/pages/LoginPage'));
 const DashboardHome = lazy(() => import('../../features/dashboard/pages/DashboardHome'));
 const DesignSystem = lazy(() => import('../../features/design-system/pages/DesignSystem'));
+const Login = lazy(() => import("./pages/Login"));
+const Home = lazy(() => import("./pages/Home"));
 
 export const guestRoutes: RouteObject[] = [
   {
     path: '/login',
     element: <LoginPage />,
+  },
+  {
+    path: "/login-v2",
+    element: <Login />,
   },
 ];
 
@@ -17,6 +23,10 @@ export const authRoutes: RouteObject[] = [
     path: '/',
     element: <DashboardHome />,
   },
+  {
+    path: "/home",
+    element: <Home />,
+  }
 ];
 
 export const publicRoutes: RouteObject[] = [
