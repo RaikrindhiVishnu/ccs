3f71c080880e686a5a420e3d8b9a186a2cb36506
bhanuja-bodapati <bhanujabodapati@gmail.com>
2026-05-24 16:44:49 +0530

    updated user directory payload changes

--- src/features/role-manager/api/userDirectoryApi.ts
+++ src/features/role-manager/api/userDirectoryApi.ts
@@ -21,7 +21,7 @@ export const userDirectoryApi = roleManagerApi.injectEndpoints({
         }),
 
         // 2. Field Officer Details
-        getFieldOfficerDetails: builder.query<any, { regional_officer_id: string | number; intelligence_officer_id: string | number; offset?: string }>({
+        getFieldOfficerDetails: builder.query<any, { state_id: string | number; region_id: string | number; regional_officer_id: string | number; intelligence_officer_id: string | number; area_id?: string | number; offset?: string }>({
             query: (body) => ({
                 url: "/userDirectory/fieldOfficers",
                 method: "POST",
@@ -33,14 +33,14 @@ export const userDirectoryApi = roleManagerApi.injectEndpoints({
         }),
 
         // 3. Agent Details
-        getAgentDetails: builder.query<any, string | number>({
-            query: (fieldOfficerId) => ({
+        getAgentDetails: builder.query<any, { state_id: string | number; region_id: string | number; area_id: string | number; field_officer_id: string | number }>({
+            query: (body) => ({
                 url: "/userDirectory/agentDetails",
                 method: "POST",
                 body: {
-                    field_officer_id: fieldOfficerId,
                     offset: "0",
                     limit: 200,
+                    ...body,
                 },
             }),
         }),
